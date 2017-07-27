b8ef1242de593ee1826a69c559d41a4571e5781b
Cameron Nicolle <cameron.nicolle@outlook.com>
2017-07-27 11:13:12 -0400

    moves config to filter

--- filter/process.go
+++ filter/process.go
@@ -2,21 +2,41 @@ package filter
 
 import (
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"os"
-	"path"
-
-	"github.com/wincam/go-filter/settings"
 )
 
+//ImageFormat defines types of images
+type ImageFormat string
+
+// supported image format identifiers
+const (
+	PNG  ImageFormat = "PNG"
+	GIF  ImageFormat = "GIF"
+	JPEG ImageFormat = "JPEG"
+	BMP  ImageFormat = "BMP"
+	TIFF ImageFormat = "TIFF"
+)
+
+//Config contains the rules that are applied when filtering
+type Config struct {
+	Input  DirectoryConfig `yaml:"input"`
+	Output DirectoryConfig `yaml:"output"`
+}
+
+//DirectoryConfig contains all data about how go-filter will interact with a directory
+type DirectoryConfig struct {
+	Directory string        `yaml:"directory"`
+	Formats   []ImageFormat `yaml:"formats,flow"`
+}
+
 //RunFilter takes a filter config and runs that filter
-func RunFilter(filter settings.FilterConfig) error {
+func RunFilter(filter Config) error {
 	if !isDirectory(filter.Input.Directory) {
 		return errors.New(filter.Input.Directory + " is not a directory.")
 	}
 
-	err := processDirectory(filter)
+	err := filter.processDirectory()
 	if err != nil {
 		return err
 	}
@@ -35,23 +55,24 @@ func isDirectory(dir string) bool {
 }
 
 //processDirectory runs a filter on a directory and all it's children
-func processDirectory(filter settings.FilterConfig) error {
+func (filter Config) processDirectory() error {
 	//get all files in directory
-	files, err := ioutil.ReadDir(filter.Input.Directory)
+	_, err := ioutil.ReadDir(filter.Input.Directory)
 	if err != nil {
 		return err
 	}
 
-	for _, file := range files {
-		if file.IsDir() {
-			fmt.Println(file.Name() + "/")
-			newFilter := filter
-			newFilter.Output.Directory = path.Join(newFilter.Output.Directory, file.Name())
-			newFilter.Input.Directory = path.Join(newFilter.Input.Directory, file.Name())
-			processDirectory(newFilter)
-		}
-		fmt.Println(file.Name())
-	}
+	/*
+		for _, file := range files {
+			if file.IsDir() {
+				fmt.Println(file.Name() + "/")
+				newFilter := filter
+				newFilter.Output.Directory = path.Join(newFilter.Output.Directory, file.Name())
+				newFilter.Input.Directory = path.Join(newFilter.Input.Directory, file.Name())
+				newFilter.processDirectory()
+			}
+			fmt.Println(file.Name())
+		}*/
 
 	return nil
 }
--- settings/settings.go
+++ settings/settings.go
@@ -7,40 +7,17 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/wincam/go-filter/filter"
 	"gopkg.in/yaml.v2"
 )
 
-//ImageFormat defines types of images
-type ImageFormat string
-
-// supported image format identifiers
-const (
-	PNG  ImageFormat = "PNG"
-	GIF  ImageFormat = "GIF"
-	JPEG ImageFormat = "JPEG"
-	BMP  ImageFormat = "BMP"
-	TIFF ImageFormat = "TIFF"
-)
-
-//FilterConfig contains the rules that are applied when filtering
-type FilterConfig struct {
-	Input  DirectoryConfig `yaml:"input"`
-	Output DirectoryConfig `yaml:"output"`
-}
-
-//DirectoryConfig contains all data about how go-filter will interact with a directory
-type DirectoryConfig struct {
-	Directory string        `yaml:"directory"`
-	Formats   []ImageFormat `yaml:"formats,flow"`
-}
-
-//YamlConfig contains all settings contained in the config yml
-type YamlConfig struct {
-	FilterConfigs []FilterConfig `yaml:"config,flow"`
+//ConfigList contains all settings contained in the config yml
+type ConfigList struct {
+	FilterConfigs []filter.Config `yaml:"config,flow"`
 }
 
 //Config is the configuration yaml instance for go-filter
-var Config YamlConfig
+var Config ConfigList
 
 //LoadConfig loads all arguments and the config yml
 func LoadConfig() error {
